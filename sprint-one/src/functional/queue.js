3d8a7e66f13ce9cbac968955ba40734b7d7db0ce
alexnitta <a.j.nitta@gmail.com>
2016-03-02 15:57:13 -0800

    Add ability for queue to remove the least recently added of 2 items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,41 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+    }
+    count--;
+
+    var lowest = count + 1;
+
+    for (var key in storage) {
+      if (key < lowest) {
+        lowest = key;
+      }
+    }
+
+    return storage[lowest];
+    return storage[size];
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+// 0: val1, 1: val2, 2: val3
+
