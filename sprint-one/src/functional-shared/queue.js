35a859d7c1f740a7a956f6ac675f25f8365d1456
alexnitta <a.j.nitta@gmail.com>
2016-03-02 20:28:36 -0800

    Add methods for enqueue and size with basic functionality

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.currentSize = 0;
+  storage.count = 0;
+  storage.lastKey = 0;
+
+  storage.enqueue = queueMethods.enqueue;
+  storage.dequeue = queueMethods.dequeue;
+  storage.size = queueMethods.size;
+
+  return storage;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function (value) {
+  this.currentSize++;
+};
+queueMethods.dequeue = function () {
 
-
+};
+queueMethods.size = function () {
+  return this.currentSize;
+};
