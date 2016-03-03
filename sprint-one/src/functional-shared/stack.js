4d640d9c0f8805b5f6973455ca30db33d04edd7a
alexnitta <a.j.nitta@gmail.com>
2016-03-02 17:26:58 -0800

    Functional shared stack now reports size of 0 after removing more items than added

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.currentSize = 0;
+
+  storage.push = stackMethods.push;
+  storage.pop = stackMethods.pop;
+  storage.size = stackMethods.size;
+
+  return storage;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function() {
+  this.currentSize++;
+};
+
+stackMethods.pop = function() {
+  if (this.currentSize > 0) {
+    this.currentSize--;
+  }
+
+};
+
+stackMethods.size = function() {
+  return this.currentSize;
+};
 
 
