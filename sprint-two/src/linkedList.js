6ae88603152a80d8b2da1a2a3439b14ef1286b4c
alexnitta <a.j.nitta@gmail.com>
2016-03-03 19:18:26 -0800

    add comments

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,50 +4,61 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create myNode var for Node to add to end of head
     var myNode = Node(value);
+    // var starts at list.head, and we will dig deeper with while loop
     var current = list.head;
-    var tail = list.tail;
 
+    // Checking for first time operation (adding to empty list.head)
     if (current === null) {
+      // if this is first value, set list.head to myNode
       list.head = myNode;
     } else {
+      // Otherwise, dig into var current to add node when current.next === null
       while (current.next) {
+        // if current.next !== null, change current to current.next (digging deeper)
         current = current.next;
       }
 
+      // when current.next === null, we will change that to myNode;
       current.next = myNode;
     }
 
+    // make list.tail to be last item added (myNode)
     list.tail = myNode;
   };
 
   list.removeHead = function() {
-    var results = list.head;
-    delete list.head;
-    list.head = results.next;
-    return results.value;
+    // storing current list.head as current
+    var current = list.head;
+    // replacing list.head with it's value from next
+    list.head = list.head.next;
+    // return old list.head value that was overwritten
+    return current.value;
   };
 
-  list.contains = function(target) { 
-
+  list.contains = function(target) {
+    // storing current list.head as current
     var current = list.head;
 
-
-
+    // if current.next isn't null (will not check last node)
     while (current.next) {
+      // check if current.value === target
       if (current.value === target) {
         return true;
       } else {
+        // if current.value !== target
         current = current.next;
       }
     }
 
+    // this checks if very last node has value === target
     if (current.value === target) {
       return true;
     } else {
       return false;
     }
-    
+
   };
 
   return list;
