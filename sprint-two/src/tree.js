0db8d4914e3244a1f7a3d7a82861029b935933b7
alexnitta <a.j.nitta@gmail.com>
2016-03-03 20:36:05 -0800

    Add ability to add children to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -14,10 +14,48 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var child = {value: value, children: []};
+  _.extend(child, treeMethods);
   this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  var wasFound = false;
+
+  var searchChildren = function (childArr) {
+
+    if (_.contains(childArr, target)) {
+      wasFound = true;
+    }
+
+    _.each(childArr.children, function(item) {
+      searchChildren(item);
+    });
+
+    // for (var i = 0; i < childArr.length; i++) {
+    //   if (childArr[i] === target) {
+    //     return true;
+    //   }
+    // }
+
+    // _.each(childArr, function (item) {
+    //   if (item.value === target) {
+    //     return true;
+    //   }
+
+    //   if (item.children.length !== 0) {
+    //     return searchChildren(item.children);
+    //   }
+
+    // });
+  };
+
+  searchChildren(this.children);
+  return wasFound;
+
 };
 
 
