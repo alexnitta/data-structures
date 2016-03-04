3091b57b47e08f49002f9e63c5dfb14c2725f0d7
alexnitta <a.j.nitta@gmail.com>
2016-03-04 10:57:51 -0800

    Pass all tests for Tree and add comments

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,59 +1,62 @@
 var Tree = function(value) {
   var newTree = {};
+
+  // give this node a value as inputted
   newTree.value = value;
 
-  // your code here
-
+  // add shared methods to each tree instance
   _.extend(newTree, treeMethods);
-  newTree.children = [];  // fix me
+
+  // create empty array to hold children
+  newTree.children = []; 
 
   return newTree;
 };
 
+// object containing methods to add to Tree class
 var treeMethods = {};
 
+// adds a node to this node's children array
 treeMethods.addChild = function(value) {
-  var child = {value: value, children: []};
-  _.extend(child, treeMethods);
+  
+  // create new instance of tree
+  var child = Tree(value);
+
+  // add instance to current node's children array
   this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+
+  // if the root node contains the value, return true
   if (this.value === target) {
     return true;
   }
 
+  // set up a binary storage variable
   var wasFound = false;
 
+  // recursively search through this.children for target = value
   var searchChildren = function (childArr) {
 
-    if (_.contains(childArr, target)) {
-      wasFound = true;
-    }
-
-    _.each(childArr.children, function(item) {
-      searchChildren(item);
+    // loop through this collection of children
+    _.each(childArr, function (item) {
+      // if any child contains value,
+      if (item.value === target) {
+        // target was found
+        wasFound = true;
+      } else { // if no child contains target,
+        // search deeper
+        searchChildren(item.children);
+      }
     });
 
-    // for (var i = 0; i < childArr.length; i++) {
-    //   if (childArr[i] === target) {
-    //     return true;
-    //   }
-    // }
-
-    // _.each(childArr, function (item) {
-    //   if (item.value === target) {
-    //     return true;
-    //   }
-
-    //   if (item.children.length !== 0) {
-    //     return searchChildren(item.children);
-    //   }
-
-    // });
   };
 
+  // initial call on children of root
   searchChildren(this.children);
+
+  // return result of search
   return wasFound;
 
 };
