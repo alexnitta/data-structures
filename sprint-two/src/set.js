93f37f3a62bef204e2c3fc998cb8a229f382d5e6
alexnitta <a.j.nitta@gmail.com>
2016-03-05 10:09:21 -0800

    Determine time complexity for Set methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,15 +6,15 @@ var Set = function() {
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { /*Time Complexity - Constant - O(1)*/
   this[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { /*Time Complexity - Linear - O(n)*/
   return _.contains(this, item);
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { /*Time Complexity - Constant - O(1)*/
   delete this[item];
 };
 
